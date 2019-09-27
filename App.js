ab5eaeb23e96176a949c3894b1ab8a8be65c1e39
aferreras <ferrerasalexander@hotmail.com>
2019-09-27 08:10:11 -0400

    Completado Integracion app platzi con expo

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store';
+import Loading from './src/sections/components/loading';
+import AppLayout from './src/app';
+type Props = {};
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider
+      store={store}
+    >
+      <PersistGate
+        loading={<Loading />}
+        persistor={persistor}
+      >
+        <AppLayout />
+      </PersistGate>
+    </Provider>
   );
 }
 
